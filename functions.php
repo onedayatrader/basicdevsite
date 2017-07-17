55be7d609ff8d93718369f7fc83878b3bee5b74d
Aiden <onedayatrader@gmail.com>
2017-07-17 21:05:50 +0100

    Folders and files added.

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description
+ *
+ * @package ${NAMESPACE}
+ * @since 1.0.0
+ * @author Aiden Potter
+ * @link https://onedayatrader.com
+ * @license GNU General Public License 2.0+
+ */
