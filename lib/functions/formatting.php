4f5dd50139172677bb888ec4481ea22d0788ba42
Aiden <onedayatrader@gmail.com>
2017-07-29 18:36:13 +0100

    files added

---
+++ lib/functions/formatting.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Description
+ *
+ * @package odat\basicdevsite\functions
+ * @since 1.0.0
+ * @author Aiden Potter
+ * @link https://onedayatrader.com
+ * @license GNU General Public License 2.0+
+ */
+
+namespace odat\basicdevsite\functions;
